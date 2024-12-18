d331f1c0a18d27ba5ec5653eb7562a962317adaf
Ali Hassan <hassan.jutt646464@gmail.com>
2024-12-18 22:32:13 +0500

    xyz

--- lib/sub_screens/course_detail.dart
+++ lib/sub_screens/course_detail.dart
@@ -46,7 +46,7 @@ class CourseDetails extends StatelessWidget {
               fullDetails,
             ),
             const SizedBox(
-              height: 10,
+              height: 10.1,
             ),
             Text('Duration',
                 style: GoogleFonts.rubik(
