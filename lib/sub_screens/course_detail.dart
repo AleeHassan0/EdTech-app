71d0fd1bd38b34edd8733c08ca8584614a01b977
Ali Hassan <hassan.jutt646464@gmail.com>
2025-01-14 21:25:17 +0500

    xyz

--- lib/sub_screens/course_detail.dart
+++ lib/sub_screens/course_detail.dart
@@ -58,7 +58,7 @@ class CourseDetails extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(left: 33, top: 60),
               child: Container(
-                height: 46,
+                height: 47,
                 width: 300,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(15),
